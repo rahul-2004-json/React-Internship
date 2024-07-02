652beff91487b9db3bb9189c2ac954235d56dd49
rahul_004 <rahulbky2004@gmail.com>
2024-07-02 12:24:44 +0530

    1st July Commit

---
+++ Thinking_in_react/ThinkinReact/src/App.jsx
@@ -0,0 +1,11 @@
+import Gallery from "./component/Gallery";
+
+export default function App() {
+  return (
+    // <> </> This empty tag is called a Fragment. Fragments let you group things without leaving any trace in the browser HTML tree.
+    <>
+      <Gallery/>
+    </>
+  )
+}
+
---
+++ Thinking_in_react/ThinkinReact/src/component/Gallery.jsx
@@ -0,0 +1,10 @@
+import Profile from "./Profile";
+
+export default function Gallery(){
+    return (
+        <>
+            <h1>Amazing Guru</h1>
+            <Profile/>
+        </>
+    )
+}
